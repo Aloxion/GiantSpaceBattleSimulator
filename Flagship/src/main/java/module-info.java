0e2c2b8467dc0ee1af80c0641a4400c6200f01b4
Aloxion <58940157+Aloxion@users.noreply.github.com>
2023-03-27 10:32:27 +0200

    Update module-info.java

--- Flagship/src/main/java/module-info.java
+++ Flagship/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 import gsbs.flagshipsystem.FlagshipControlSystem;
 import gsbs.flagshipsystem.FlagshipPlugin;
 
-module Player {
+module Flagship {
     requires Common;
     provides gsbs.common.services.IPlugin with FlagshipPlugin;
     provides gsbs.common.services.IProcess with FlagshipControlSystem;

f85c9423b8d355eeada1cd9b931fc41cc01720d1
Mathias Gredal <matg@universal-robots.com>
2023-04-23 21:35:09 +0200

    Create health component

---
+++ Common/src/main/java/gsbs/common/components/Health.java
@@ -0,0 +1,21 @@
+package gsbs.common.components;
+
+public class Health extends Component {
+    private int healthPoints;
+
+    public Health(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void removeHealthPoints(int amount) {
+        healthPoints = Math.max(0, healthPoints - amount);
+    }
+
+    public boolean isDead() {
+        return healthPoints == 0;
+    }
+}

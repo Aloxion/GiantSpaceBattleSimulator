236519858f9270109477eb950a6b3c9c12e95f26
v-nemeth <91267701+v-nemeth@users.noreply.github.com>
2023-04-25 16:23:00 +0200

    dev-8 Implemented a bullet sprite for bullet control system

--- Common/src/main/java/gsbs/common/services/IWeapon.java
+++ Common/src/main/java/gsbs/common/services/IWeapon.java
@@ -1,8 +1,9 @@
 package gsbs.common.services;
 
 import gsbs.common.data.GameData;
+import gsbs.common.data.World;
 import gsbs.common.entities.Entity;
 
 public interface IWeapon {
-    void fire(Entity entity, GameData gameData);
+    void fire(Entity entity, GameData gameData, World world);
 }

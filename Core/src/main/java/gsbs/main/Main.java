6dcf39f7309daaeac26699355911f8a00ff08714
Aloxion <58940157+Aloxion@users.noreply.github.com>
2023-05-03 21:34:29 +0200

    Update Main.java

--- Core/src/main/java/gsbs/main/Main.java
+++ Core/src/main/java/gsbs/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
         var cfg = new Configuration();
         cfg.setTitle("Giant Space Battle Simulator");
         cfg.setWidth(1200);
-        cfg.setHeight(600);
+        cfg.setHeight(800);
         cfg.setFullScreen(false);
         cfg.setVsync(true);
         cfg.setBackgroundColor(new Color(0, 0, 0, 1));

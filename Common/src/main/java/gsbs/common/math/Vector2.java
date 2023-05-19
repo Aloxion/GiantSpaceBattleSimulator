68b5ccc09f6a239d6cdc6b8b6db4b1b14e458af9
Mathias Gredal <matg@universal-robots.com>
2023-05-19 02:01:02 +0200

    Add title to start screen

--- Common/src/main/java/gsbs/common/math/Vector2.java
+++ Common/src/main/java/gsbs/common/math/Vector2.java
@@ -13,6 +13,7 @@ public class Vector2 {
         this.x = x;
         this.y = y;
     }
+
     public boolean equals(Vector2 other) {
         return (this.x == other.x && this.y == other.y);
     }
@@ -70,4 +71,7 @@ public class Vector2 {
         return (float) Math.sqrt(x * x + y * y);
     }
 
+    public float direction() {
+        return (float) Math.atan2(this.y, this.x);
+    }
 }

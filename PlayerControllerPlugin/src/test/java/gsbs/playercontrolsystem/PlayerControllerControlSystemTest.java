c90b075af409ca9beb0d09ebd94ce9af1d5e9377
v-nemeth <viand21@student.sdu.dk>
2023-05-23 19:22:29 +0200

    Start on player-controller-unit-test

---
+++ PlayerControllerPlugin/src/test/java/gsbs/playercontrolsystem/PlayerControllerControlSystemTest.java
@@ -0,0 +1,36 @@
+package gsbs.playercontrolsystem;
+
+import gsbs.common.data.GameData;
+import gsbs.common.data.World;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerControllerControlSystemTest {
+
+    @BeforeAll
+    static void init(){
+
+    }
+
+    @BeforeEach
+    void beforeTest(){
+
+    }
+
+    @Test
+    void process() {
+        GameData gd = new GameData(0);
+        World w = new World();
+
+        //Setup a player entity with the correct components
+
+        //Set a gamekey
+        //Test that the component acts accordingly to the gameKey
+
+        //repeat
+
+    }
+}

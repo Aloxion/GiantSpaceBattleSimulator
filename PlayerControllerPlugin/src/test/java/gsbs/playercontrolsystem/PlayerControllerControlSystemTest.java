8ae9e16da863a0f501a2142eed8762d797cf459d
Busch31 <frederikbusch31@gmail.com>
2023-05-25 16:19:21 +0200

    tests added

--- PlayerControllerPlugin/src/test/java/gsbs/playercontrolsystem/PlayerControllerControlSystemTest.java
+++ PlayerControllerPlugin/src/test/java/gsbs/playercontrolsystem/PlayerControllerControlSystemTest.java
@@ -1,36 +1,70 @@
 package gsbs.playercontrolsystem;
 
 import gsbs.common.data.GameData;
+import gsbs.common.data.GameKeys;
 import gsbs.common.data.World;
-import org.junit.jupiter.api.BeforeAll;
+import gsbs.common.entities.Entity;
+import gsbs.common.events.EventType;
+import gsbs.common.events.PlayerControlEvent;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerControllerControlSystemTest {
-
-    @BeforeAll
-    static void init(){
-
-    }
+    private PlayerControllerControlSystem systemUnderTest;
+    private GameData gameData;
+    private World world;
 
     @BeforeEach
-    void beforeTest(){
-
+    void setUp() {
+        long nvgContext = 0L;
+        systemUnderTest = new PlayerControllerControlSystem();
+        gameData = new GameData(nvgContext);
+        world = new World();
     }
 
     @Test
-    void process() {
-        GameData gd = new GameData(0);
-        World w = new World();
-
-        //Setup a player entity with the correct components
-
-        //Set a gamekey
-        //Test that the component acts accordingly to the gameKey
-
-        //repeat
-
+    void testProcess_keyLeftDown() {
+        gameData.getKeys().setKey(GameKeys.Keys.LEFT, true);
+        systemUnderTest.process(gameData, world);
+        assertTrue(gameData.getKeys().isDown(GameKeys.Keys.LEFT));
     }
+
+    @Test
+    void testProcess_keyRightDown() {
+        gameData.getKeys().setKey(GameKeys.Keys.RIGHT, true);
+        systemUnderTest.process(gameData, world);
+        assertTrue(gameData.getKeys().isDown(GameKeys.Keys.RIGHT));
+    }
+
+    @Test
+    void testProcess_keyUpDown() {
+        gameData.getKeys().setKey(GameKeys.Keys.UP, true);
+        systemUnderTest.process(gameData, world);
+        assertTrue(gameData.getKeys().isDown(GameKeys.Keys.UP));
+    }
+
+    @Test
+    void testProcess_keySpaceDown() {
+        gameData.getKeys().setKey(GameKeys.Keys.SPACE, true);
+        systemUnderTest.process(gameData, world);
+        assertTrue(gameData.getKeys().isDown(GameKeys.Keys.SPACE));
+    }
+
+    @Test
+    void testProcess_keyWeaponCycleUpDown() {
+        gameData.getKeys().setKey(GameKeys.Keys.WEAPON_CYCLE_UP, true);
+        systemUnderTest.process(gameData, world);
+        assertTrue(gameData.getKeys().isDown(GameKeys.Keys.WEAPON_CYCLE_UP));
+    }
+
+    @Test
+    void testProcess_keyWeaponCycleDownDown() {
+        gameData.getKeys().setKey(GameKeys.Keys.WEAPON_CYCLE_DOWN, true);
+        systemUnderTest.process(gameData, world);
+        assertTrue(gameData.getKeys().isDown(GameKeys.Keys.WEAPON_CYCLE_DOWN));
+    }
+
 }
+
